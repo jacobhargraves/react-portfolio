eeebe26d3c1648c640465356c984c74c137c9510
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-09-11 13:11:34 -0600

    added name form in contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,12 +5,16 @@ export default function Contact() {
       <div className='container mt-5 card w-50'>
         <h1 className="text-center">Contact Me</h1>
         <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+          <label for="nameInput" className="form-label">Name</label>
+          <input type="text" className="form-control" id="nameInput" placeholder="ex. John Doe" aria-label="Name"></input>
         </div>
         <div className="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="emailInput" className="form-label">Email address</label>
+            <input type="email" class="form-control" id="emailInput" placeholder="name@example.com"></input>
+        </div>
+        <div className="mb-3">
+            <label for="messageInput" class="form-label">Message</label>
+            <textarea class="form-control" id="messageInput" rows="4"></textarea>
         </div>
       </div>
     );
